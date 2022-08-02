afcd6ecf7d7cfbfe7ce5bdc539249fed2bb76589
Marsel <maergon@yandex.ru>
2022-08-02 17:28:00 +0500

    Настроил фильтрацию по параметрам

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/NoteUseCase.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/NoteUseCase.kt
@@ -1,7 +1,11 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
 
+/**
+ * Main UseCase
+ */
 class NoteUseCase(
-    val getNotes: GetNotesUseCase,
-    val deleteNoteUseCase: DeleteNoteUseCase
+    val getNotesUseCase: GetNotesUseCase,
+    val deleteNoteUseCase: DeleteNoteUseCase,
+    val addNoteUseCase: AddNoteUseCase
 ) {
 }
